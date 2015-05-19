a397ba1cd437ffc53ca592c08e471fd1467ecc84
lenamasek <lena.masek@gmail.com>
2015-05-19 00:24:08 -0500

    new theme!

--- js/main.js
+++ js/main.js
@@ -1,22 +1,66 @@
-var elements = document.getElementsByTagName('script')
+window.onload = function() {
 
-Array.prototype.forEach.call(elements, function(element) {
-  if (element.type.indexOf('math/tex') != -1) {
-     // Extract math markdown
-     var textToRender = element.innerText || element.textContent;
+    var $menuIcon = document.getElementsByClassName('menu-icon')[0],
+        $offCanva = document.getElementsByClassName('off-canvas')[0];
+        $siteWrap = document.getElementsByClassName('site-wrapper')[0];
 
-     // Create span for KaTeX
-     var katexElement = document.createElement('span');
+    $menuIcon.addEventListener('click', function() {
+        toggleClass($menuIcon, 'close');
+        toggleClass($offCanva, 'toggled');
+        toggleClass($siteWrap, 'open');
+    }, false);
 
-     // Support inline and display math
-     if (element.type.indexOf('mode=display') != -1){
-       katexElement.className += "math-display";
-       textToRender = '\\displaystyle {' + textToRender + '}';
-     } else {
-       katexElement.className += "math-inline";
-     }
+    $menuIcon.addEventListener('mouseenter', function() {
+        addClass($menuIcon, 'hover');
+    });
 
-     katex.render(textToRender, katexElement);
-     element.parentNode.insertBefore(katexElement, element);
-  }
-});
+    $menuIcon.addEventListener('mouseleave', function() {
+        removeClass($menuIcon, 'hover');
+    });
+
+    function addClass(element, className) {
+        element.className += " " + className;
+    }
+
+    function removeClass(element, className) {
+        // Capture any surrounding space characters to prevent repeated
+        // additions and removals from leaving lots of spaces.
+        var classNameRegEx = new RegExp("\\s*" + className + "\\s*");
+        element.className = element.className.replace(classNameRegEx, " ");
+    }
+
+    function toggleClass(element, className) {
+        if (!element || !className) {
+            return;
+        }
+
+        if (element.className.indexOf(className) === -1) {
+            addClass(element, className);
+        } else {
+            removeClass(element, className);
+        }
+    }
+
+    // Open Twitter/share in a Pop-Up
+    var $popup = document.getElementsByClassName('popup')[0];
+    if (!$popup) {
+        return;
+    }
+    $popup.addEventListener('click', function(e) {
+        e.preventDefault()
+        var width  = 575,
+            height = 400,
+            left   = (document.documentElement.clientWidth  - width)  / 2,
+            top    = (document.documentElement.clientHeight - height) / 2,
+            url    = this.href,
+            opts   = 'status=1' +
+                     ',width='  + width  +
+                     ',height=' + height +
+                     ',top='    + top    +
+                     ',left='   + left;
+
+        window.open(url, 'twitter', opts);
+
+        return false;
+    });
+}
